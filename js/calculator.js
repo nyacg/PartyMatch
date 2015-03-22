629937a5fbb20cca981819acf5061b5e157b113f
vesko_ski <edv_junior@abv.bg>
2015-03-22 15:15:02 +0000

    working calculator (maybe ?!)

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,7 @@
 var mydata;
 var translate = new Map();
 
-
-//Need data structure like one below !
-var theThing = {Slug: "globalTaxAvoidanceInfoExchange", Aliance: 1, Con: -1};
-var theThing2 = {Slug: "increaseIncomeTax", Aliance: 1, Con: 1};
-var dataStructure = [theThing, theThing2];
-console.log(theThing);
-//End data structure
+var fileRob = {"globalTaxAvoidanceInfoExchange" : 1, "jobseecerAllowance" : -1};
 
 $(document).ready(function(){
 	translate.set('Aliance', "Alliance");
@@ -28,32 +22,20 @@ $(document).ready(function(){
 		console.log("getting data failed");
 	});
 });
-function getScore(responseArray){
+function getScore(sentFromRob){
 	var score = 0;
 	var scoreParty = new Map();
 	var agreed = new Array();
-	for (var i = responseArray.length - 1; i >= 0; i--) {
-		for (var k = mydata.length - 1; k >= 0; k--) {
-			if (responseArray[i].Slug == mydata[k].Slug) {
-				for (var key in responseArray[i]) {
-					if(key != 'Slug'){
-						score += (responseArray[i][key] * mydata[k][key]);
-						scoreParty.set(key, score);
-						if(responseArray[i][key] * mydata[k][key] > 0.5){
-							agreed.push(responseArray[i]['Slug']);
-						}
-					}
-				};
-				score = 0;
-			};
-		};
-	//example of getting score of party
-	console.log(scoreParty.get("Con"));
-	console.log(agreed);
-	//how to get all scores
-	for (var key of scoreParty.keys()) {
-  		console.log(translate.get(key), scoreParty.get(key));
+	for (var key of translate.keys()){
+		for(var responce in sentFromRob){
+			for(var i = mydata.length - 1; i >0; i--){
+				if(mydata[i]['Slug'] == responce){
+					score += (sentFromRob[responce] * mydata[i][key]);
+				}
+			}
+		}
+		scoreParty.set(translate.get(key), score);
+		score = 0;
 	}
 	return scoreParty;
-	};
 }

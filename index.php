19d453b90c0e1195fffb94ad42ce88d28cc52f23
Robert Chandler <robert.j.h.chandler@googlemail.com>
2015-03-22 12:34:01 +0000

    Icons etc

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Party Match</title>
+
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" sizes="192x192" href="img/icon.png">
+
     <link rel="stylesheet" type="text/css" href="css/jTinder.css">
     <link rel="stylesheet" type="text/css" href="css/ui.css">
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">

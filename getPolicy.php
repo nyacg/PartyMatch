5fe2846273526fba3d2a8f2809f1348ba308cfed
Robert Chandler <robert.j.h.chandler@googlemail.com>
2015-03-22 09:31:14 +0000

    getPolicy updated for increased usefulness

--- getPolicy.php
+++ getPolicy.php
@@ -1,8 +1,18 @@
 <?php
 	$file = fopen("./data/policy.csv", "r") or die("Unable to open file!");
 	$data = Array();
+	$keys = Array();
+	$i = 0;
 	while(! feof($file)) {
-		$data[] = fgetcsv($file);
+		$line = fgetcsv($file);
+		if($i == 0){
+			// first time get the column headings
+			$i++;
+			$keys = $line;
+		} else {
+			// normally add the headings to the row and append this to the array of all rows
+			$data[] = array_combine($keys, $line);
+		}
 	}
 
 	echo json_encode($data);
